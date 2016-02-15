fb6d75ad0ca25d913c0f55866e7a567a4836cfcb
bluebear <bluebear@bluebearui-MacBook-Pro.local>
2016-02-16 00:37:40 +0900

    modify server

--- index.js
+++ index.js
@@ -14,13 +14,24 @@ console.log("http server listening on %d", port)
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
+
+wss.broadcast = function(data) {
+  for (var i in this.clients)
+    this.clients[i].send(data);
+};
+
+
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
     ws.send(JSON.stringify(new Date()), function() {  })
   }, 1000)
 
-  console.log("websocket connection open")
+    console.log("websocket connection open")
 
+  ws.on('message', function(message) {
+    wss.broadcast(message);
+  });
+    
   ws.on("close", function() {
     console.log("websocket connection close")
     clearInterval(id)
