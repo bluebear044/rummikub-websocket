a8232d2bc155833817e981bc8ac475d0dbea7a9d
bluebear <bluebear@bluebearui-MacBook-Pro.local>
2016-02-17 01:31:43 +0900

    modify

--- index.js
+++ index.js
@@ -9,30 +9,38 @@ app.use(express.static(__dirname + "/"))
 var server = http.createServer(app)
 server.listen(port)
 
-console.log("http server listening on %d", port)
+console.log("http server listening on %d", port);
 
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+console.log("websocket server created");
 
+var count = 0;
+var clients = {};
 
 wss.broadcast = function(data) {
-  for (var i in this.clients) {
-
-console.log("@@ : " + data);      
-    this.clients[i].send(JSON.stringify(data));
-  }
+    console.log("broadcast msg : " + data);
+    for (var i in this.clients) {
+        this.clients[i].send(JSON.stringify(data));
+    }
 };
 
 
 wss.on("connection", function(ws) {
-
-    console.log("websocket connection open")
-
-  ws.on('message', function(message) {
-    wss.broadcast(message);
-  });
+    // Specific id for this client & increment count
+    var id = "GUEST_" + count++;
+    // Store the connection method so we can loop through & contact all clients
+    clients[id] = ws;
     
-  ws.on("close", function() {
-    console.log("websocket connection close")
-  })
+    console.log("websocket connection open");
+    wss.broadcast((new Date()) + "<br/>" + id + ' connected.');
+
+    ws.on('message', function(message) {
+       wss.broadcast(id + " : " + message);
+    });
+    
+    ws.on("close", function() {
+       console.log("websocket connection close");
+       wss.broadcast((new Date()) + "<br/>" + id + ' disconnected.');
+       delete clients[id];
+   })
 })
