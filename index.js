81c6d1a1638b43afb3eaa52318cf3b5e4a0238ed
bluebear <bluebear@bluebearui-MacBook-Pro.local>
2016-02-20 10:51:19 +0900

    modify

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var port = process.env.PORT || 5000
 //constant
 var fs = require('fs');
 eval(fs.readFileSync('./constant.js')+'');
+eval(fs.readFileSync('./rummikub.js')+'');
 
 //start server
 app.use(express.static(__dirname + "/"))
@@ -19,9 +20,10 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created");
 
 //static variable
+var rummikub = new Rummikub();
 var connectCount = 0;
 var clients = {};
-var startFlag = false;
+var gamePlayingFlag = false;
 
 //broadcast client
 wss.broadcast = function(data) {
@@ -33,12 +35,16 @@ wss.broadcast = function(data) {
 
 //connect client
 wss.on("connection", function(ws) {
+    // Turn Flag
+    var myTurnFlag = false;
     // Specific id for this client & increment connectCount
     var id = "GUEST_" + UTIL.random4digit();
     connectCount++;
     // Store the connection method so we can loop through & contact all clients
     clients[id] = ws;
-    
+    // User Setting
+    rummikub.users.push(new User(id));
+  
     initConnect();
 
     ws.on('message', function(message) {
@@ -74,7 +80,7 @@ wss.on("connection", function(ws) {
     }
 
     function boardInfo() {
-        return {"connectCount" : connectCount, "startFlag" : startFlag};
+        return {"connectCount" : connectCount, "gamePlayingFlag" : gamePlayingFlag, "myTurnFlag" : myTurnFlag};
     }
 
     function initConnect() {
@@ -84,9 +90,14 @@ wss.on("connection", function(ws) {
     }
 
     function processStart() {
+
+        rummikub.initializeGame();
+        console.log(rummikub.users);
+
+        gamePlayingFlag = true;
+
         wss.broadcast(makeCommand(CMD.START));
         wss.broadcast(MESSAGE.MSG_START);
-        startFlag = true;
         wss.broadcast(makeCommand( CMD.INFO, boardInfo() ));
     }
 
@@ -96,9 +107,10 @@ wss.on("connection", function(ws) {
     }
 
     function processExit() {
+        gamePlayingFlag = false;
+
         wss.broadcast(makeCommand(CMD.EXIT));
         wss.broadcast(MESSAGE.MSG_EXIT);
-        startFlag = false;
         wss.broadcast(makeCommand( CMD.INFO, boardInfo() ));
     }
 
@@ -108,11 +120,17 @@ wss.on("connection", function(ws) {
 
     function processDisconnect() {
         console.log("websocket connection close");
-        wss.broadcast(UTIL.printNowDate() + "<br/>" + id + MESSAGE.MSG_DISCONNECT);
         //client & connect count delete
         delete clients[id];
         connectCount--;
-        
+        //User setting
+        rummikub.removeUser(id);
+
+        if(gamePlayingFlag == true) {
+            processExit();
+        }
+
+        wss.broadcast(UTIL.printNowDate() + "<br/>" + id + MESSAGE.MSG_DISCONNECT);
         wss.broadcast(makeCommand( CMD.INFO, boardInfo() ));
     }
 
---
+++ rummikub.js
@@ -0,0 +1,65 @@
+//Rummikub class
+function Rummikub () {
+	this.scores = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
+	this.colors = ['red','blue','yellow','black','red','blue','yellow','black'];
+	this.tiles = [];
+	this.users = [];
+}
+
+Rummikub.prototype.shuffle = function(o) {
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+Rummikub.prototype.initializeGame = function() {
+	
+	this.initializeTiles();
+
+	for(var idx in this.users) {
+  		this.initializeTilesToUser(this.users[idx]);
+	}
+}
+
+Rummikub.prototype.initializeTiles = function() {
+	for(var i in this.colors) {
+	  for(var j in this.scores) {
+	  	this.tiles.push(new Tile(this.scores[j], this.colors[i], false));
+	  }
+	}
+	// add joker tiles
+	this.tiles.push(new Tile('30', 'red', true));
+	this.tiles.push(new Tile('30', 'black', true));
+
+	this.shuffle(this.tiles);
+};
+
+Rummikub.prototype.initializeTilesToUser = function(user) {
+	user.own = [];
+	for(var idx=0; idx < 14; idx++) {
+  		user.own[idx] = this.tiles.pop();
+	}
+};
+
+Rummikub.prototype.removeUser = function(id) {
+	for(var idx in this.users) {
+		if(id == this.users[idx].id) {
+			delete this.users[idx];
+		}
+	}
+};
+
+//User class
+function User (id) {
+	this.id = id;
+	this.registerYN = false;
+	this.use = [];
+	this.own = [];
+	this.myTurnFlag = false;
+}
+
+//Tile class
+function Tile (score, color, isJoker) {
+	this.score = score;
+	this.color = color;
+	this.isJoker = isJoker;
+}
